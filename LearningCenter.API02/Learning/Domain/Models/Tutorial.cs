a8b309da1d4116e7bd1cabc58353cf4249acf501
Jaime Escobar Aguirre <pcccjesc@upc.edu.pe>
2023-10-14 19:51:28 -0500

    feat(tutorial): Created Tutorial Model

---
+++ LearningCenter.API02/Learning/Domain/Models/Tutorial.cs
@@ -0,0 +1,9 @@
+﻿namespace LearningCenter.API02.Learning.Domain.Models;
+
+public class Tutorial
+{
+    //Id, Name, Descripcion 
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
