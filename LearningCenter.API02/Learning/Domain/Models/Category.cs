ad39ff62b6d1a68b720a7a5556e509a0382d75fb
Jaime Escobar Aguirre <pcccjesc@upc.edu.pe>
2023-10-14 20:05:12 -0500

    feat Categories: Implemented Property-based relationship with Tutorials

--- LearningCenter.API02/Learning/Domain/Models/Category.cs
+++ LearningCenter.API02/Learning/Domain/Models/Category.cs
@@ -5,5 +5,10 @@ public class Category
     //patron de diseño identity field
     public int Id { get; set; }
     public string Name { get; set; }
-    
+   /*ya tenemos tutorial y Category lo que falta es relacionarlos , representacion
+   a traves de una clase */
+   public IList<Tutorial> Tutorials { get; set; } = new List<Tutorial>();
+  
+   
+
 }
--- LearningCenter.API02/Learning/Domain/Models/Tutorial.cs
+++ LearningCenter.API02/Learning/Domain/Models/Tutorial.cs
@@ -6,4 +6,12 @@ public class Tutorial
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    //establecemos una relacion de asociacion de uno a muchos 
+    //Relationship 
+    //asp.net Core (entity Framework ) -- persistencia 
+    //necesita que se le indique que atributo usa la contraparte , a nivel de tabla
+    public int CategoryId { get; set; }
+   
+    // poo 
+    public Category Category { get; set; }
 }
