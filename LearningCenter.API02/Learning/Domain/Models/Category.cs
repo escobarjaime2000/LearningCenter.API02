b4cc6c62d1d696aec31bdd82961b72d64cd47a05
Jaime Escobar Aguirre <pcccjesc@upc.edu.pe>
2023-10-14 19:48:24 -0500

    feat(categories): created Category Model

---
+++ LearningCenter.API02/Learning/Domain/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace LearningCenter.API02.Learning.Domain.Models;
+
+public class Category
+{
+    //patron de diseño identity field
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+}
