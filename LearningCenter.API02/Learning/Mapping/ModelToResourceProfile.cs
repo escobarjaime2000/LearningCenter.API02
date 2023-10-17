c93cb65365487942d03b95637193de763221833c
Jaime Escobar Aguirre <pcccjesc@upc.edu.pe>
2023-10-17 07:43:48 -0500

    feat(Category): Implemented Object Mapping for Category

---
+++ LearningCenter.API02/Learning/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LearningCenter.API02.Learning.Domain.Models;
+using LearningCenter.API02.Learning.Resources;
+
+namespace LearningCenter.API02.Learning.Mapping;
+
+public class ModelToResourceProfile: Profile
+{
+    protected ModelToResourceProfile()
+    {
+        CreateMap<Category, CategoryResource>();
+    }
+}
--- LearningCenter.API02/Learning/Resources/SaveCategoryResource.cs
+++ LearningCenter.API02/Learning/Resources/SaveCategoryResource.cs
@@ -4,4 +4,5 @@ public class SaveCategoryResource
 {
     public string Name { get; set; }
     
+    
 }
