e6f1b754abd0881cf255d15b0c79127f61158acc
Jaime Escobar Aguirre <pcccjesc@upc.edu.pe>
2023-10-22 07:54:05 -0500

    feat(tutorials): Added Incoming and outgoing Transport Obcject

---
+++ LearningCenter.API02/Learning/Resources/SaveTutorialResource.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningCenter.API02.Learning.Resources;
+
+public class SaveTutorialResource
+{
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+    
+    [MaxLength(120)]
+    public string Description { get; set; }
+    [Required]
+    [MaxLength]
+    public int CategoryId { get; set; }
+}
---
+++ LearningCenter.API02/Learning/Resources/TutorialResource.cs
@@ -0,0 +1,15 @@
+﻿namespace LearningCenter.API02.Learning.Resources;
+
+public class TutorialResource
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public CategoryResource Category { get; set; }
+    //se relaciona Category a traves de CategoryResource que es property
+    //esto es un resource no es un  modelo 
+    //se elige categoryResource y no category 
+    //Category tiene la conexion Tutorials que el cliente no debe ver
+    //en cambio resource tiene su propia informacion, conveniente.
+    
+}
