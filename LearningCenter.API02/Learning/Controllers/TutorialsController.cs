054762d41778f1ae8414c589d873d81a84a0ddac
Jaime Escobar Aguirre <pcccjesc@upc.edu.pe>
2023-10-26 09:35:06 -0500

    feat(tutorials) : Added Persistence constraint and relationship and  update tutorialControllers

---
+++ LearningCenter.API02/Learning/Controllers/TutorialsController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using LearningCenter.API02.Learning.Resources;
+using Microsoft.AspNetCore.Mvc;
+using ZstdSharp.Unsafe;
+
+namespace LearningCenter.API02.Learning.Controllers;
+//tutorial controller es descendiente de ControllerBase  y ademas es decorado con [ApiController]
+//especificamos la ruta con la cual se asocia 
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class TutorialsController: ControllerBase
+
+{
+    private readonly ITutorialService _tutorialService;
+    private readonly IMapper _mapper;
+    
+}
+public async Task<IEnumerable<TutorialResource>> GetAllAsync()
+{
+    var tutorials = await _tutorialServices.ListAsync();
+    var resources = _mapper.Map<IEnumerable<Tutorial>, IEnumerable<TutorialResource>>(tutorials);
+    return resources;
+}
--- LearningCenter.API02/Learning/Persistence/Context/AppDbContext.cs
+++ LearningCenter.API02/Learning/Persistence/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using LearningCenter.API02.Learning.Domain.Models;
+using LearningCenter.API02.Learning.Resources;
 using LearningCenter.API02.Shared.Extensions;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,14 +12,34 @@ public class AppDbContext:DbContext
     {
         
     }
-    public DbSet<Category> Categories { set; get; }
+    public DbSet<Category> Categories { get; set; }
+    //sesion 10- parte 02--
+    public DbSet<Tutorial> Tutorials { get; set; }
     protected override void OnModelCreating(ModelBuilder Builder)
     {
         base.OnModelCreating(Builder);
+        
+        //Categories
         Builder.Entity<Category>().ToTable("Categories");
         Builder.Entity<Category>().HasKey(p => p.Id);
         Builder.Entity<Category>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
         Builder.Entity<Category>().Property(p => p.Name).IsRequired().HasMaxLength(30);
+        
+        
+        
+        //RelationShip
+
+        Builder.Entity<Category>().HasMany(p => p.Tutorials)
+                                  .WithOne(p=>p.Category)
+                                  .HasForeignKey(p=>p.CategoryId);
+              
+        
+        //tutorials estableciendo la relacion de uno a muchos 
+        Builder.Entity<Tutorial>().ToTable("Tutorials");
+        Builder.Entity<Tutorial>().HasKey(p =>p.Id);
+        Builder.Entity<Tutorial>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        Builder.Entity<Tutorial>().Property(p => p.Name).IsRequired().HasMaxLength(50);
+        Builder.Entity<Tutorial>().Property(p => p.Description).IsRequired().HasMaxLength(120);
         
         //aply naming conventions 
         Builder.UseSnakeCaseNamingConvention();
